de1bbf4dca095ab25d51c5db60e5ab68f5fb2463
Bharath Kumar Ravichandran <bharathkumarravichandran@gmail.com>
2019-08-27 19:38:38 +0530

    Change entrypoint command in app_entry script

--- scripts/react_app_entry.sh
+++ scripts/react_app_entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sleep 2
+sleep 20
 
 echo ""
 echo ""
@@ -10,7 +10,7 @@ echo ""
 echo "    *****   Entering react_app_entry     ***** "
 
 echo "    ***** Starting the React application *****"
-npm run build -b 0.0.0.0:3000
+node prodServer.js -b 0.0.0.0:3000
 echo ""
 echo " ###############################################"
 echo ""
